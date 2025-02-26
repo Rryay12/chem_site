7d9e3e0a629a42fa5995713168482ae4988b32d4
Rona Sando <ard@Mac.lan>
2025-02-27 01:31:29 +0900

    hello

--- reaction.py
+++ reaction.py
@@ -2,83 +2,73 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import solve_ivp
 
-class reaction:
+class Reaction:
     
     def __init__(self):
         self.molecule_info = {}
-        self.k = 1
+        self.k = 1  # Rate constant for the reaction
         plt.switch_backend('Agg') 
         
-    def add_molecule(self,name:str,degree:int,initial_concentration:float,is_product:bool,equilibrium_concentration:float = 0.0):
-        self.molecule_info[name] = {"degree": degree if is_product else -degree,
-                                    "initial_concentration":initial_concentration,
-                                    "is_product":is_product,
-                                    "equilibrium_concentration":equilibrium_concentration}
+    def add_molecule(self, name: str, degree: int, initial_concentration: float):
+        """Add molecules (reactants or products) with their degree (stoichiometric coefficient) and initial concentration."""
+        self.molecule_info[name] = {
+            "degree": degree,
+            "initial_concentration": initial_concentration
+        }
         
-    
-    def calc_k(self) -> None:
-        
-        current_conc = self.get_eq_conc_list()
-        degree_list = self.get_degree_list()
-        
-        return np.prod([(current_conc[i] + 0.001)**degree_list[i] for i in range(len(current_conc))])
-    
-    def set_k(self,k:int) -> None:
+    def set_k(self, k: float):
+        """Set the rate constant for the reaction."""
         self.k = k
         
     def get_degree_list(self):
+        """Get the list of degrees (stoichiometric coefficients) for the reaction."""
         return [self.molecule_info[key]["degree"] for key in self.molecule_info.keys()]
 
     def get_init_conc_list(self):
+        """Get the list of initial concentrations for the reaction."""
         return [self.molecule_info[key]["initial_concentration"] for key in self.molecule_info.keys()]
 
-    def get_eq_conc_list(self):
-        return [self.molecule_info[key]["equilibrium_concentration"] for key in self.molecule_info.keys()]
-
-    def reaction(self, t,current_conc):
+    def reaction(self, t, current_conc):
+        """Return the rate of change of concentrations of all molecules."""
         return_list = []
         degree_list = self.get_degree_list()
         
-        epsilon = 1e-10
+        epsilon = 1e-10  # To avoid division by zero or negative concentrations
         rate = self.k * np.prod([(current_conc[i] + epsilon) ** degree_list[i] for i in range(len(current_conc))])
         
+        # Update concentrations based on the rate and degree (reaction order)
         for i in range(len(current_conc)):
             return_list.append(degree_list[i] * rate)
         
         return return_list
     
-    def simulate(self,t_span,num_points):
+    def simulate(self, t_span, num_points):
+        """Simulate the reaction over a time span."""
         t_eval = np.linspace(t_span[0], t_span[1], num_points)
-        
         sol = solve_ivp(self.reaction, t_span, self.get_init_conc_list(), t_eval=t_eval, method='LSODA')
         return sol.t, sol.y
     
-    def make_graph(self,t_span = None,num_points=100,link = ""):
-        if t_span==None:
-            t_span = (0,int(10/self.k))
+    def make_graph(self, t_span=None, num_points=100, link=""):
+        """Generate and save a graph showing the concentrations of all molecules over time."""
+        if t_span is None:
+            t_span = (0, int(10 / self.k))  # Default time span based on rate constant
         
+        t, y = self.simulate(t_span, num_points)
         
-        t,y = self.simulate(t_span,num_points)
+        plt.figure(figsize=(8, 5))
         
-        
-        plt.figure(figsize=(8,5))
-        
-        for no,molecule in enumerate(self.molecule_info.keys()):
+        for no, molecule in enumerate(self.molecule_info.keys()):
             plt.plot(t, y[no], label=molecule)
-            
+        
         plt.xlabel('Time (s)')
         plt.ylabel('Concentration (M)')
-        plt.title('Multi-Species Reaction Simulation')
+        plt.title('Unidirectional Reaction Simulation')
         plt.legend()
         plt.grid()
         
-        plt.savefig(link+"/reaction.png")
+        plt.savefig(link + "/reaction.png")
         
     def reset(self):
+        """Reset the reaction to initial state."""
         self.molecule_info = {}
         self.k = 1
-        
-    
-
-
-        
