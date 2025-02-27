eb4c90ba43ddbbbf0a516b8d7521a85d58e3a2bb
Rona Sando <ard@MacBook-Air-2.local>
2025-02-27 15:16:47 +0900

    hello

--- app.py
+++ app.py
@@ -17,30 +17,44 @@ def index():
         reactants = request.form.getlist("reactant_name")
         reactant_degrees = request.form.getlist("reactant_degree")
         reactant_concentrations = request.form.getlist("reactant_concentration")
-        
-        # Reaction constant k
-        k = request.form.getlist("k")[0]
+        reactant_eq_concentrations = request.form.getlist("reactant_eq_conc")  # New field
 
         products = request.form.getlist("product_name")
         product_degrees = request.form.getlist("product_degree")
         product_concentrations = request.form.getlist("product_concentration")
+        product_eq_concentrations = request.form.getlist("product_eq_conc")  # New field
+
+        # Reaction constant k
+        k = request.form.get("k", 1.0)  # Default value of 1.0 if not provided
 
         # Fill in the reaction data for reactants and products
         reaction_data["reactants"] = [
-            {"name": r, "degree": d, "concentration": c}
-            for r, d, c in zip(reactants, reactant_degrees, reactant_concentrations) if r.strip()
+            {"name": r, "degree": d, "concentration": c, "eq_conc": eq_c}
+            for r, d, c, eq_c in zip(reactants, reactant_degrees, reactant_concentrations, reactant_eq_concentrations) if r.strip()
         ]
         reaction_data["products"] = [
-            {"name": p, "degree": d, "concentration": c}
-            for p, d, c in zip(products, product_degrees, product_concentrations) if p.strip()
+            {"name": p, "degree": d, "concentration": c, "eq_conc": eq_c}
+            for p, d, c, eq_c in zip(products, product_degrees, product_concentrations, product_eq_concentrations) if p.strip()
         ]
         
         # Add reactants and products to the Python reaction model
         for reactant in reaction_data["reactants"]:
-            python_reaction.add_molecule(name=reactant["name"], degree=int(reactant["degree"]), initial_concentration=float(reactant["concentration"]), is_product=False)
+            python_reaction.add_molecule(
+                name=reactant["name"], 
+                degree=int(reactant["degree"]), 
+                initial_concentration=float(reactant["concentration"]),
+                eq_conc=float(reactant["eq_conc"]), 
+                is_product=False
+            )
         
         for product in reaction_data["products"]:
-            python_reaction.add_molecule(name=reactant["name"], degree=int(reactant["degree"]), initial_concentration=float(reactant["concentration"]), is_product=True)
+            python_reaction.add_molecule(
+                name=product["name"], 
+                degree=int(product["degree"]), 
+                initial_concentration=float(product["concentration"]),
+                eq_conc=float(product["eq_conc"]),  # Pass eq_conc
+                is_product=True
+            )
         
         # Set the reaction constant k
         python_reaction.set_k(float(k))
--- reaction.py
+++ reaction.py
@@ -7,44 +7,59 @@ class reaction:
     def __init__(self):
         self.molecule_info = {}
         self.k = 1
+        self.K_eq = 1
         plt.switch_backend('Agg') 
         
-    def add_molecule(self,name:str,degree:int,initial_concentration:float,is_product:bool):
+    def add_molecule(self,name:str,degree:int,initial_concentration:float,is_product:bool,eq_conc:float = 1):
         self.molecule_info[name] = {"degree": degree if is_product else -degree,
                                     "initial_concentration":initial_concentration,
-                                    "is_product":is_product}
+                                    "is_product":is_product,
+                                    "eq_conc":eq_conc}
         
-    
+    def set_k_eq(self):
+        epsilon = 1e-10 
+        eq_conc = self.get_init_conc_list()
+        self.K_eq = np.prod([(eq_conc[i] + epsilon) ** eq_conc[i] for i in range(len(eq_conc))])
+        
     def set_k(self,k:int) -> None:
         self.k = k
         
     def get_degree_list(self):
         return [self.molecule_info[key]["degree"] for key in self.molecule_info.keys()]
 
+    def get_eq_conc_list(self):
+        return [self.molecule_info[key]["eq_conc"] for key in self.molecule_info.keys()]
+
     def get_init_conc_list(self):
         return [self.molecule_info[key]["initial_concentration"] for key in self.molecule_info.keys()]
 
-    def reaction(self, t,current_conc):
+    def reaction(self, t, current_conc):
+        self.set_k_eq()
         return_list = []
         degree_list = self.get_degree_list()
-        
-        epsilon = 1e-10
-        rate = self.k * np.prod([(current_conc[i] + epsilon) ** -degree_list[i] for i in range(len(current_conc)) if degree_list[i] < 0])
+        epsilon = 1e-10  
+        forward_rate = self.k * np.prod([(current_conc[i] + epsilon) ** abs(degree_list[i]) 
+                                        for i in range(len(current_conc)) if degree_list[i] < 0])
+        reverse_rate = (self.k / self.K_eq) * np.prod([(current_conc[i] + epsilon) ** abs(degree_list[i]) 
+                                                for i in range(len(current_conc)) if degree_list[i] > 0])
+        net_rate = forward_rate - reverse_rate
         
         for i in range(len(current_conc)):
-            return_list.append(degree_list[i] * rate)
-        
+            return_list.append(degree_list[i] * net_rate)
+            
         return return_list
+
+
     
     def simulate(self,t_span,num_points):
         t_eval = np.linspace(t_span[0], t_span[1], num_points)
         
-        sol = solve_ivp(self.reaction, t_span, self.get_init_conc_list(), t_eval=t_eval, method='LSODA')
+        sol = solve_ivp(self.reaction, t_span, self.get_init_conc_list(), t_eval=t_eval, method='BDF')
         return sol.t, sol.y
     
     def make_graph(self,t_span = None,num_points=100,link = ""):
         if t_span==None:
-            t_span = (0,int(10/self.k))
+            t_span = (0,10/self.k)
         
         
         t,y = self.simulate(t_span,num_points)
